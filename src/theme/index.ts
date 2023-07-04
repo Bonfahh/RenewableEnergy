70709dc6bbf8ec26a21e510c09a40b877e4efe3f
Matheus Bonfiglioli <bonfiglioli.mat@gmail.com>
2023-07-03 22:22:42 -0300

    Added folder structure, theme, fonts and path mapping

---
+++ src/theme/index.ts
@@ -0,0 +1,29 @@
+export default {
+  FONT_FAMILY: {
+    REGULAR: 'Sora-Regular',
+    MEDIUM: 'Sora-Medium',
+    SEMI_BOLD: 'Sora-SemiBold',
+    BOLD: 'Sora-Bold',
+  },
+  FONT_SIZE: {
+    10: 10,
+    11: 11,
+    12: 12,
+    16: 16,
+    17: 17,
+    18: 18,
+    14: 24,
+    24: 24,
+  },
+  COLORS: {
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+    PURPLE: '#770FDF',
+    PURPLE_LIGHT: '#F7EFFF',
+    GREEN: '#0FDF8F',
+    RED: '#EE8688',
+    GREY_700: '#A0A0A0',
+    GREY_500: '#CFCFCF',
+    GREY_100: '#F4F4F4',
+  },
+};
